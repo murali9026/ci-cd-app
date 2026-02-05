f6f79b89e4a99264493f8e4fc7f6f4c75262f2b9
murali9026 <murali.krishna@opsera.io>
2026-02-04 22:47:38 -0800

    Initial commit - CI/CD app

---
+++ README.md
@@ -0,0 +1,22 @@
+\# CI/CD App
+
+
+
+This is a minimal Java Maven app used to demonstrate:
+
+\- CI build
+
+\- Unit tests
+
+\- Artifact generation
+
+\- Security scanning
+
+\- Deployment via Opsera
+
+
+
+Build:
+
+
+
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Opsera CI/CD pipeline 🚀");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
